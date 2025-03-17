65d425de3d1dba991b94fd583e475252493307de
Martin Kirov <fas7blas7@gmail.com>
2025-03-17 22:22:56 +0200

    Adding LargestNumber Task

---
+++ 2-LargestNumber/LargestNumberOutOfThree.cs
@@ -0,0 +1,25 @@
+﻿namespace _2_LargestNumber
+{
+    internal class LargestNumberOutOfThree
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            if(firstNumber > secondNumber && firstNumber > thirdNumber)
+            {
+                Console.WriteLine($"{firstNumber}");
+            }
+            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+            {
+                Console.WriteLine($"{secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"{thirdNumber}");
+            }
+        }
+    }
+}

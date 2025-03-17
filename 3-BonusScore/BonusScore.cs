84fb860b657d2686650cbe4f29a664c6d0037893
Martin Kirov <fas7blas7@gmail.com>
2025-03-17 22:23:42 +0200

    Adding BonusScore Task

---
+++ 3-BonusScore/BonusScore.cs
@@ -0,0 +1,28 @@
+﻿namespace _3_BonusScore
+{
+    internal class BonusScore
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            double bonusScore = 0;
+
+            if (number >= 0 && number <= 3)
+            {
+                bonusScore = number + 5;
+                Console.WriteLine($"{bonusScore}");
+            }
+            else if(number >= 4 && number <= 6)
+            {
+                bonusScore = number + 15;
+                Console.WriteLine($"{bonusScore}");
+            }
+            else if (number >= 7 && number <= 9)
+            {
+                bonusScore = number + 20;
+                Console.WriteLine($"{bonusScore}");
+            }
+        }
+    }
+}

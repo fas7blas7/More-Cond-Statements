8264db71dd76961a520e1d25335380b967e88cfa
Martin Kirov <fas7blas7@gmail.com>
2025-03-18 22:25:35 +0200

    Adding FoodOrDrink task

---
+++ 4-FoodOrDrink/FoodOrDrink.cs
@@ -0,0 +1,23 @@
+﻿namespace _4_FoodOrDrink
+{
+    internal class FoodOrDrink
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+            if (product is "curry" or "noodles" or "sushi" or "spaghetti" or "bread")
+            {
+                Console.WriteLine("food");
+            }
+            else if (product is "coffee" or "tea" or "water" or "juice")
+            {
+                Console.WriteLine("drink");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}

bffd2bf0cbb0e1599ec2771866b309ddd648b071
Martin Kirov <fas7blas7@gmail.com>
2025-03-18 22:27:16 +0200

    Adding InvalidNumber task

---
+++ 5-InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace _5_InvalidNumber
+{
+    internal class InvalidNumber
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0 || number >= 100 && number <= 200)
+            {            
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}

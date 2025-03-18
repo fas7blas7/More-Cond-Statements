8e9354a29be7745940b26988c39e1eb82476d357
Martin Kirov <fas7blas7@gmail.com>
2025-03-18 22:28:17 +0200

    Adding DayOfWeekAsNumber task

---
+++ 6-DayOfWeekAsNumber/DayOfWeekAsNumber.cs
@@ -0,0 +1,43 @@
+﻿namespace _6_DayOfWeekAsNumber
+{
+    internal class DayOfWeekAsNumber
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            if (dayOfWeek == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            if (dayOfWeek == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            if (dayOfWeek == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            if (dayOfWeek == 4)
+            {
+                Console.WriteLine("Thursday");
+            }
+            if (dayOfWeek == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            if (dayOfWeek == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            if (dayOfWeek == 7)
+            {
+                Console.WriteLine("Sunday");
+            }
+            else if (dayOfWeek < 1 || dayOfWeek > 7)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}

a69a12e2864d43680a2b8b996a9388ab83ef58cc
Martin Kirov <fas7blas7@gmail.com>
2025-03-19 20:30:37 +0200

    Adding VowelOrConsunant task

---
+++ 7-VowelOrConsunant/VowelOrConsonant.cs
@@ -0,0 +1,19 @@
+﻿namespace _7_VowelOrConsonant
+{
+    internal class VowelOrConsonant
+    {
+        static void Main(string[] args)
+        {
+            char letter = char.Parse(Console.ReadLine());
+
+            if(letter is 'A' or 'a' or 'E' or 'e' or 'I' or 'i' or 'O' or 'o' or 'U' or 'u')
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+        }
+    }
+}

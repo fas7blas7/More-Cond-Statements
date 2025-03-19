47a1ba2112700a7405ca11564e274727ab995756
Martin Kirov <fas7blas7@gmail.com>
2025-03-19 20:31:48 +0200

    Adding Cinema task

---
+++ 9-Cinema/Cinema.cs
@@ -0,0 +1,33 @@
+﻿namespace _9_Cinema
+{
+    internal class Cinema
+    {
+        static void Main(string[] args)
+        {
+            string typeOfMovie = Console.ReadLine();
+            int countOfRows = int.Parse(Console.ReadLine());
+            int countOfSeats = int.Parse(Console.ReadLine());
+            
+            double priceOfTicket = 0;
+            double totalCountOfSeats = 0;
+            if (typeOfMovie == "Premiere")
+            {
+                priceOfTicket = 12.00;
+                totalCountOfSeats = countOfRows * countOfSeats;
+                Console.WriteLine($"{totalCountOfSeats * priceOfTicket:f2}");
+            }
+            else if (typeOfMovie == "Normal")
+            {
+                priceOfTicket = 7.50;
+                totalCountOfSeats = countOfRows * countOfSeats;
+                Console.WriteLine($"{totalCountOfSeats * priceOfTicket:f2}");
+            }
+            else if (typeOfMovie == "Discount")
+            {
+                priceOfTicket = 5.00;
+                totalCountOfSeats = countOfRows * countOfSeats;
+                Console.WriteLine($"{totalCountOfSeats * priceOfTicket:f2}");
+            }
+        }
+    }
+}
